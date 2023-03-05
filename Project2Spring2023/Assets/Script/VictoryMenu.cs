57f0bba3f5a20ed02a7e718a0d100813d9021388
Random-Hometale <karobinson@knights.ucf.edu>
2023-03-05 17:57:01 -0500

    Snake

---
+++ Project2Spring2023/Assets/Script/VictoryMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
